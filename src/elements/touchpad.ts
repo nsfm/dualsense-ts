a9bcdd9fc034bd61f9f98248a1be87a450fee167
Nate Dube <nate@ndube.com>
2022-04-13 14:05:01 -0700

    Modelling out parent inputs

--- src/elements/touchpad.ts
+++ src/elements/touchpad.ts
@@ -1,15 +1,24 @@
 import { Axis } from "./axis";
 import { Momentary } from "./momentary";
 import { InputId } from "./input_ids";
+import { Input, InputParams } from "./input";
 
-export class Touchpad {
+export class Touchpad extends Input<Touchpad> {
+  public readonly state: Touchpad = this;
+  public readonly defaultState: Touchpad = this;
+
+  // TODO: the Input.active function needs to be tied to the child inputs
+  // TODO: Handling for child inputs within Input?
+
   public readonly x1;
   public readonly y1;
   public readonly x2;
   public readonly y2;
   public readonly button;
 
-  constructor() {
+  constructor(params: InputParams) {
+    super(params);
+
     this.button = new Momentary({
       id: InputId.TouchpadButton,
       icon: "[__]",
