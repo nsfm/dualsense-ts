b6ca2fd29f930d3ef0be86bba99add0bf5a23e8a
Nate Dube <nate@ndube.com>
2022-06-27 23:23:07 -0700

    Rework HID report handling

--- src/hid/dualsense_hid.ts
+++ src/hid/dualsense_hid.ts
@@ -1,61 +1,7 @@
 import { EventEmitter } from "events";
 
-import { HIDProvider } from "./hid_provider";
+import { HIDProvider, DualsenseHIDState } from "./hid_provider";
 import { InputId } from "./ids";
-
-export interface DualsenseHIDState {
-  [InputId.LeftAnalogX]: number;
-  [InputId.LeftAnalogY]: number;
-  [InputId.RightAnalogX]: number;
-  [InputId.RightAnalogY]: number;
-  [InputId.LeftTrigger]: number;
-  [InputId.RightTrigger]: number;
-  [InputId.Triangle]: boolean;
-  [InputId.Circle]: boolean;
-  [InputId.Cross]: boolean;
-  [InputId.Square]: boolean;
-  [InputId.Dpad]: number;
-  [InputId.Up]: boolean;
-  [InputId.Down]: boolean;
-  [InputId.Left]: boolean;
-  [InputId.Right]: boolean;
-  [InputId.RightAnalogButton]: boolean;
-  [InputId.LeftAnalogButton]: boolean;
-  [InputId.Options]: boolean;
-  [InputId.Create]: boolean;
-  [InputId.RightTriggerButton]: boolean;
-  [InputId.LeftTriggerButton]: boolean;
-  [InputId.RightBumper]: boolean;
-  [InputId.LeftBumper]: boolean;
-  [InputId.Playstation]: boolean;
-  [InputId.TouchButton]: boolean;
-  [InputId.Mute]: boolean;
-  [InputId.Status]: boolean;
-  [InputId.TouchX0]: number;
-  [InputId.TouchY0]: number;
-  [InputId.TouchContact0]: boolean;
-  [InputId.TouchId0]: number;
-  [InputId.TouchX1]: number;
-  [InputId.TouchY1]: number;
-  [InputId.TouchContact1]: boolean;
-  [InputId.TouchId1]: number;
-  [InputId.GyroX]: number;
-  [InputId.GyroY]: number;
-  [InputId.GyroZ]: number;
-  [InputId.AccelX]: number;
-  [InputId.AccelY]: number;
-  [InputId.AccelZ]: number;
-}
-
-// Maps a HID input of 0...n to -1...1
-export function mapAxis(value: number, max: number = 255): number {
-  return (2 / max) * Math.max(0, Math.min(max, value)) - 1;
-}
-
-// Maps a HID input of 0...255 to 0...1
-export function mapTrigger(value: number): number {
-  return (1 / 255) * Math.max(0, Math.min(255, value));
-}
 
 /**
  * Coordinates a HIDProvider and tracks the latest HID state.
@@ -107,76 +53,13 @@ export class DualsenseHID extends EventEmitter {
 
   constructor(private provider: HIDProvider) {
     super();
-    provider.onData = this.process.bind(this);
+
+    provider.onData = (state: DualsenseHIDState) => {
+      this.state = state;
+      this.emit("input", state);
+    };
+
     provider.onError = this.handleError.bind(this);
-  }
-
-  /**
-   * Unpacks a Dualsense HID report.
-   */
-  private process(buffer: Buffer): void {
-    // Bluetooth buffer starts with an extra byte
-    const report = buffer.subarray(this.provider.wireless ? 2 : 1);
-
-    const { state } = this;
-    state[InputId.LeftAnalogX] = mapAxis(report.readUint8(0));
-    state[InputId.LeftAnalogY] = -mapAxis(report.readUint8(1));
-    state[InputId.RightAnalogX] = mapAxis(report.readUint8(2));
-    state[InputId.RightAnalogY] = -mapAxis(report.readUint8(3));
-    state[InputId.LeftTrigger] = mapTrigger(report.readUint8(4));
-    state[InputId.RightTrigger] = mapTrigger(report.readUint8(5));
-    // 6 is a sequence byte
-    const mainButtons = report.readUint8(7);
-    state[InputId.Triangle] = (mainButtons & 128) > 0;
-    state[InputId.Circle] = (mainButtons & 64) > 0;
-    state[InputId.Cross] = (mainButtons & 32) > 0;
-    state[InputId.Square] = (mainButtons & 16) > 0;
-    state[InputId.Dpad] = (mainButtons << 4) >> 4;
-    state[InputId.Up] = state[InputId.Dpad] < 2 || state[InputId.Dpad] === 7;
-    state[InputId.Down] = state[InputId.Dpad] > 2 && state[InputId.Dpad] < 6;
-    state[InputId.Left] = state[InputId.Dpad] > 4 && state[InputId.Dpad] < 8;
-    state[InputId.Right] = state[InputId.Dpad] > 0 && state[InputId.Dpad] < 4;
-    const miscButtons = report.readUint8(8);
-    state[InputId.LeftTriggerButton] = (miscButtons & 4) > 0;
-    state[InputId.RightTriggerButton] = (miscButtons & 8) > 0;
-    state[InputId.LeftBumper] = (miscButtons & 1) > 0;
-    state[InputId.RightBumper] = (miscButtons & 2) > 0;
-    state[InputId.Create] = (miscButtons & 16) > 0;
-    state[InputId.Options] = (miscButtons & 32) > 0;
-    state[InputId.LeftAnalogButton] = (miscButtons & 64) > 0;
-    state[InputId.RightAnalogButton] = (miscButtons & 128) > 0;
-    const lastButtons = report.readUint8(9);
-    state[InputId.Playstation] = (lastButtons & 1) > 0;
-    state[InputId.TouchButton] = (lastButtons & 2) > 0;
-    state[InputId.Mute] = (lastButtons & 4) > 0;
-    // The other 5 bits are unused
-    // 5 reserved bytes
-    state[InputId.GyroX] = report.readUint16LE(15);
-    state[InputId.GyroY] = report.readUint16LE(17);
-    state[InputId.GyroZ] = report.readUint16LE(19);
-    state[InputId.AccelX] = report.readUint16LE(21);
-    state[InputId.AccelY] = report.readUint16LE(23);
-    state[InputId.AccelZ] = report.readUint16LE(25);
-    // 4 bytes for sensor timestamp (32LE)
-    // 1 reserved byte
-    state[InputId.TouchId0] = report.readUint8(32) & 0x7f;
-    state[InputId.TouchContact0] = (report.readUint8(32) & 0x80) === 0;
-    state[InputId.TouchX0] = mapAxis(
-      (report.readUint16LE(33) << 20) >> 20,
-      1920
-    );
-    state[InputId.TouchY0] = mapAxis(report.readUint16LE(34) >> 4, 1080);
-    state[InputId.TouchId1] = report.readUint8(36) & 0x7f;
-    state[InputId.TouchContact1] = (report.readUint8(36) & 0x80) === 0;
-    state[InputId.TouchX1] = mapAxis(
-      (report.readUint16LE(37) << 20) >> 20,
-      1920
-    );
-    state[InputId.TouchY1] = mapAxis(report.readUint16LE(38) >> 4, 1080);
-    // 12 reserved bytes
-    state[InputId.Status] = (report.readUint8(53) & 4) > 0;
-
-    this.emit("input", state);
   }
 
   private handleError(error: unknown): void {
--- src/hid/web_hid_provider.ts
+++ src/hid/web_hid_provider.ts
@@ -1,4 +1,10 @@
-import { HIDProvider } from "./hid_provider";
+import {
+  HIDProvider,
+  DualsenseHIDState,
+  InputId,
+  mapAxis,
+  mapTrigger,
+} from "./hid_provider";
 
 export class WebHIDProvider extends HIDProvider {
   private device?: HIDDevice;
@@ -18,10 +24,17 @@ export class WebHIDProvider extends HIDProvider {
           return this.onError(new Error(`No controllers available`));
         }
 
-        devices[0].open().then(() => {
-          this.device = devices[0];
-          this.device.addEventListener("inputreport", ({ data }) => { this.onData(data) })
-        }).catch((err: Error) => { this.onError(err) })
+        devices[0]
+          .open()
+          .then(() => {
+            this.device = devices[0];
+            this.device.addEventListener("inputreport", ({ data }) => {
+              this.onData(data);
+            });
+          })
+          .catch((err: Error) => {
+            this.onError(err);
+          });
 
         if (controllers[0].interface === -1) this.wireless = true;
       })
@@ -41,5 +54,65 @@ export class WebHIDProvider extends HIDProvider {
         this.wireless = false;
       });
     }
+  }
+
+  process(buffer: DataView): DualsenseHIDState {
+    // Bluetooth buffer starts with an extra byte
+    const report = new DataView(buffer.buffer, this.wireless ? 2 : 1);
+
+    const mainButtons = report.getUint8(7);
+    const miscButtons = report.getUint8(8);
+    const lastButtons = report.getUint8(9);
+    const dpad = (mainButtons << 4) >> 4;
+
+    return {
+      [InputId.LeftAnalogX]: mapAxis(report.getUint8(0)),
+      [InputId.LeftAnalogY]: -mapAxis(report.getUint8(1)),
+      [InputId.RightAnalogX]: mapAxis(report.getUint8(2)),
+      [InputId.RightAnalogY]: -mapAxis(report.getUint8(3)),
+      [InputId.LeftTrigger]: mapTrigger(report.getUint8(4)),
+      [InputId.RightTrigger]: mapTrigger(report.getUint8(5)),
+      [InputId.Triangle]: (mainButtons & 128) > 0,
+      [InputId.Circle]: (mainButtons & 64) > 0,
+      [InputId.Cross]: (mainButtons & 32) > 0,
+      [InputId.Square]: (mainButtons & 16) > 0,
+      [InputId.Dpad]: dpad,
+      [InputId.Up]: dpad < 2 || dpad === 7,
+      [InputId.Down]: dpad > 2 && dpad < 6,
+      [InputId.Left]: dpad > 4 && dpad < 8,
+      [InputId.Right]: dpad > 0 && dpad < 4,
+      [InputId.LeftTriggerButton]: (miscButtons & 4) > 0,
+      [InputId.RightTriggerButton]: (miscButtons & 8) > 0,
+      [InputId.LeftBumper]: (miscButtons & 1) > 0,
+      [InputId.RightBumper]: (miscButtons & 2) > 0,
+      [InputId.Create]: (miscButtons & 16) > 0,
+      [InputId.Options]: (miscButtons & 32) > 0,
+      [InputId.LeftAnalogButton]: (miscButtons & 64) > 0,
+      [InputId.RightAnalogButton]: (miscButtons & 128) > 0,
+      [InputId.Playstation]: (lastButtons & 1) > 0,
+      [InputId.TouchButton]: (lastButtons & 2) > 0,
+      [InputId.Mute]: (lastButtons & 4) > 0,
+      [InputId.GyroX]: report.getUint16(15, true),
+      [InputId.GyroY]: report.getUint16(17, true),
+      [InputId.GyroZ]: report.getUint16(19, true),
+      [InputId.AccelX]: report.getUint16(21, true),
+      [InputId.AccelY]: report.getUint16(23, true),
+      [InputId.AccelZ]: report.getUint16(25, true),
+      [InputId.TouchId0]: report.getUint8(32) & 0x7f,
+      [InputId.TouchContact0]: (report.getUint8(32) & 0x80) === 0,
+      [InputId.TouchX0]: mapAxis(
+        (report.getUint16(33, true) << 20) >> 20,
+        1920
+      ),
+      [InputId.TouchY0]: mapAxis(report.getUint16(34, true) >> 4, 1080),
+      [InputId.TouchId1]: report.getUint8(36) & 0x7f,
+      [InputId.TouchContact1]: (report.getUint8(36) & 0x80) === 0,
+      [InputId.TouchX1]: mapAxis(
+        (report.getUint16(37, true) << 20) >> 20,
+        1920
+      ),
+      [InputId.TouchY1]: mapAxis(report.getUint16(38, true) >> 4, 1080),
+      [InputId.Status]: (report.getUint8(53) & 4) > 0,
+    };
   }
 }
